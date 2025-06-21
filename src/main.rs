2d63f48a3c190310b32309dd62dd8c4a804f6c6f
nathanljones <github.o6h95@passinbox.com>
2025-06-21 13:26:21 +0100

    Block can now move up, down, left & right

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ use bevy::prelude::*;
 #[derive(Component)]
 struct Block;
 
+#[derive(Component)]
+#[require(Transform)]
+struct Position(Vec2);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup,(spawn_camera,spawn_block))
-        .add_systems(Update,(move_block))
+        .add_systems(Update, (move_block,project_positions))
         .run();
 }
 fn spawn_camera(mut commands: Commands){
@@ -23,18 +26,27 @@ fn spawn_block(  mut commands: Commands,
     let colour = Color::Srgba(BLUE);
     let mesh = meshes.add(shape);
     let material = materials.add(colour);
-    commands.spawn((Block, Mesh2d(mesh),MeshMaterial2d(material),Transform::from_xyz(0.0,0.0,0.0)));
+    commands.spawn((Block, Mesh2d(mesh),MeshMaterial2d(material),Position(Vec2::new(0.,0.))));
 
 }
 
-fn move_block(  mut block: Query<&mut Transform, With<Block>>,  keyboard_input: Res<ButtonInput<KeyCode>>, ){
-    if let Ok(mut transform) = block.single_mut() {
+fn move_block(  mut block: Query<&mut Position, With<Block>>,  keyboard_input: Res<ButtonInput<KeyCode>>, ){
+    if let Ok(mut position) = block.single_mut() {
         if keyboard_input.pressed(KeyCode::ArrowUp) {
-            transform.translation.y = 100.;
+            position.0.y += 10.0;
         } else if keyboard_input.pressed(KeyCode::ArrowDown) {
-            transform.translation.y = -100.;
-        } else {
-            transform.translation.y = 0.;
+            position.0.y -= 10.0;
+        }else if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            position.0.x -= 10.0;
         }
+        else if keyboard_input.pressed(KeyCode::ArrowRight) {
+            position.0.x += 10.0;
+        }
+    }
+}
+
+fn project_positions(mut block_position: Query<(&mut Transform, &Position), With<Block>>){
+    for (mut transform, position) in &mut block_position {
+        transform.translation = position.0.extend(0.);
     }
 }
