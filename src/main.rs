979ec9addfed31aa3ba516ad2bbb319df6d330f3
nathanljones <github.o6h95@passinbox.com>
2025-08-10 12:24:20 +0100

    Move snake all the time - needs moving out of update

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,24 @@ struct SnakeSegments(Vec<Entity>);
 #[derive(Component)]
 #[require(Transform)]
 struct Position(Vec2);
-
+#[derive(Default)]
+enum SnakeDirection {
+    #[default]
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+}
+#[derive(Resource, Default)]
+struct CurrentSnakeDirection {
+    snake_direction: SnakeDirection,
+}
 fn main() {
     App::new()
+        .init_resource::<CurrentSnakeDirection>()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (spawn_camera, spawn_snake_head, spawn_snake_body))
-        .add_systems(Update, (move_snake, project_positions))
+        .add_systems(Update, (change_snake_direction, move_snake,project_positions))
         .run();
 }
 fn spawn_camera(mut commands: Commands) {
@@ -53,47 +65,84 @@ fn spawn_snake_body(
     let material = materials.add(colour);
     commands.spawn((
         SnakeSegment,
-        Mesh2d(mesh),
-        MeshMaterial2d(material),
+        Mesh2d(mesh.clone()),
+        MeshMaterial2d(material.clone()),
         Position(Vec2::new(0., -20.)),
     ));
+    commands.spawn((
+        SnakeSegment,
+        Mesh2d(mesh),
+        MeshMaterial2d(material),
+        Position(Vec2::new(0., -40.)),
+    ));
+}
+
+fn change_snake_direction(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut current_snake_direction: ResMut<CurrentSnakeDirection>,
+){
+    if keyboard_input.pressed(KeyCode::ArrowUp) {
+        current_snake_direction.snake_direction = SnakeDirection::UP;
+    } else if keyboard_input.pressed(KeyCode::ArrowDown) {
+        current_snake_direction.snake_direction = SnakeDirection::DOWN;
+    } else if keyboard_input.pressed(KeyCode::ArrowLeft) {
+        current_snake_direction.snake_direction = SnakeDirection::LEFT;
+    } else if keyboard_input.pressed(KeyCode::ArrowRight) {
+        current_snake_direction.snake_direction = SnakeDirection::RIGHT;
+    }
 }
 
 fn move_snake(
     mut snake_head: Query<&mut Position, (With<SnakeHead>)>,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut snake_segments: Query<&mut Position, (With<SnakeSegment>,Without<SnakeHead>)>,
+    current_snake_direction: Res<CurrentSnakeDirection>,
     window_query: Query<&Window, With<PrimaryWindow>>,
 ) -> Result {
     let window = window_query.single()?;
+    let mut snake_head_pos = Vec2::new(0., 0.);
     if let Ok(mut snake_head_position) = snake_head.single_mut() {
-        if keyboard_input.pressed(KeyCode::ArrowUp) {
-            if snake_head_position.0.y > window.height() / 2.0 {
-                snake_head_position.0.y = -1.0 * window.height() / 2.0;
-            } else {
-                snake_head_position.0.y += 10.0;
+        match current_snake_direction.snake_direction {
+            SnakeDirection::UP => {
+                if snake_head_position.0.y > window.height() / 2.0 {
+                    snake_head_position.0.y = -1.0 * window.height() / 2.0;
+                } else {
+                    snake_head_position.0.y += 10.0;
+                }
             }
-        } else if keyboard_input.pressed(KeyCode::ArrowDown) {
-            if snake_head_position.0.y < -1.0 * window.height() / 2.0 {
-                snake_head_position.0.y = window.height() / 2.0;
-            } else {
-                snake_head_position.0.y -= 10.0;
+            SnakeDirection::DOWN => {
+                if snake_head_position.0.y < -1.0 * window.height() / 2.0 {
+                    snake_head_position.0.y = window.height() / 2.0;
+                } else {
+                    snake_head_position.0.y -= 10.0;
+                }
             }
-        } else if keyboard_input.pressed(KeyCode::ArrowLeft) {
-            if snake_head_position.0.x < -1.0 * window.width() / 2.0 {
-                snake_head_position.0.x = window.width() / 2.0;
-            } else {
-                snake_head_position.0.x -= 10.0
-            };
-        } else if keyboard_input.pressed(KeyCode::ArrowRight) {
-            if snake_head_position.0.x > window.width() / 2.0 {
-                snake_head_position.0.x = -1.0 * window.width() / 2.0;
-            } else {
-                snake_head_position.0.x += 10.0
-            };
-        }else{
-            snake_head_position.0.x += 0.0;
-            snake_head_position.0.y += 0.0;
+            SnakeDirection::LEFT => {
+                if snake_head_position.0.x < -1.0 * window.width() / 2.0 {
+                    snake_head_position.0.x = window.width() / 2.0;
+                } else {
+                    snake_head_position.0.x -= 10.0
+                };
+            }
+            SnakeDirection::RIGHT => {
+                if snake_head_position.0.x > window.width() / 2.0 {
+                    snake_head_position.0.x = -1.0 * window.width() / 2.0;
+                } else {
+                    snake_head_position.0.x += 10.0
+                };
+            }
+            _ => {
+                snake_head_position.0.x += 0.0;
+                snake_head_position.0.y += 0.0;
+            }
         }
+/*        let snake_head_pos = snake_head_position.0;
+
+           let mut temp_pos = snake_head_pos;
+            for mut snake_segment in &mut snake_segments.iter_mut() {
+                let mut current_pos = snake_segment.0;
+                snake_segment.0 = temp_pos;
+                temp_pos = current_pos;
+            }*/
     }
     Ok(())
 }
