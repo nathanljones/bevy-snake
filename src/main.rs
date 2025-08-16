eb46198ca282fc729cd2b0581a1804dde51e5289
nathanljones <github.o6h95@passinbox.com>
2025-08-16 16:48:02 +0100

    Move the snake into a grdi based system. Note the positioning is a bit off. Refactoring and magical numbers will be removed later

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
-use std::time::Duration;
-use bevy::color::palettes::css::{BLUE, GREEN};
+use bevy::color::palettes::css::{BLUE, GREEN, LIGHT_GREEN};
 use bevy::prelude::*;
 use bevy::time::common_conditions::on_timer;
 use bevy::window::PrimaryWindow;
+use std::time::Duration;
 
-
+#[derive(Resource)]
+struct GameBoard {
+    width: u32,
+    height: u32,
+    cell_size: f32,
+}
 #[derive(Component)]
 struct SnakeHead;
 
 #[derive(Component)]
 struct SnakeSegment;
 
-
-
 #[derive(Component)]
 #[require(Transform)]
 struct Position(Vec2);
+
+#[derive(Component)]
+struct GridLocation(Vec2);
 #[derive(Default)]
 enum SnakeDirection {
     #[default]
@@ -32,12 +38,31 @@ fn main() {
     App::new()
         .init_resource::<CurrentSnakeDirection>()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (spawn_camera, spawn_snake_head, spawn_snake_body))
-        .add_systems(Update, (change_snake_direction, project_positions))
+        .add_systems(
+            Startup,
+            (
+                spawn_camera,
+                spawn_snake_head,
+                //spawn_snake_body,
+                setup_board,
+            ),
+        )
+        .add_systems(
+            Update,
+            (change_snake_direction, project_positions, draw_board, project_board),
+        )
         //.add_systems(Update, change_speed)
         .add_systems(Update,move_snake.run_if(on_timer(Duration::from_millis(150))))
-        .insert_resource(Time::<Fixed>::from_seconds(0.15))
+        .insert_resource(Time::<Fixed>::from_seconds(2.0))
         .run();
+}
+fn setup_board(mut commands: Commands) {
+    let board = GameBoard {
+        width: 10,
+        height: 10,
+        cell_size: 20.0,
+    };
+    commands.insert_resource(board);
 }
 fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
@@ -55,7 +80,7 @@ fn spawn_snake_head(
         SnakeHead,
         Mesh2d(mesh),
         MeshMaterial2d(material),
-        Position(Vec2::new(0., 0.)),
+        GridLocation(Vec2::new(5., 5.)),
     ));
 }
 fn spawn_snake_body(
@@ -84,7 +109,7 @@ fn spawn_snake_body(
 fn change_snake_direction(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut current_snake_direction: ResMut<CurrentSnakeDirection>,
-){
+) {
     if keyboard_input.pressed(KeyCode::ArrowUp) {
         current_snake_direction.snake_direction = SnakeDirection::Up;
     } else if keyboard_input.pressed(KeyCode::ArrowDown) {
@@ -97,57 +122,56 @@ fn change_snake_direction(
 }
 
 fn move_snake(
-    mut snake_head: Query<&mut Position, With<SnakeHead>>,
-    mut snake_segments: Query<&mut Position, (With<SnakeSegment>,Without<SnakeHead>)>,
+    mut snake_head: Query<&mut GridLocation, With<SnakeHead>>,
+    mut snake_segments: Query<&mut Position, (With<SnakeSegment>, Without<SnakeHead>)>,
     current_snake_direction: Res<CurrentSnakeDirection>,
     window_query: Query<&Window, With<PrimaryWindow>>,
 ) -> Result {
-    let window = window_query.single()?;
     if let Ok(mut snake_head_position) = snake_head.single_mut() {
         let snake_head_pos = snake_head_position.0;
         match current_snake_direction.snake_direction {
-            SnakeDirection::Up => {
-                if snake_head_position.0.y > window.height() / 2.0 {
-                    snake_head_position.0.y = -window.height() / 2.0;
-                } else {
-                    snake_head_position.0.y += 20.0;
-                }
-            }
-            SnakeDirection::Down => {
-                if snake_head_position.0.y < -window.height() / 2.0 {
-                    snake_head_position.0.y = window.height() / 2.0;
-                } else {
-                    snake_head_position.0.y -= 20.0;
-                }
-            }
-            SnakeDirection::Left => {
-                if snake_head_position.0.x < -window.width() / 2.0 {
-                    snake_head_position.0.x = window.width() / 2.0;
-                } else {
-                    snake_head_position.0.x -= 20.0
-                };
-            }
-            SnakeDirection::Right => {
-                if snake_head_position.0.x > window.width() / 2.0 {
-                    snake_head_position.0.x = -window.width() / 2.0;
-                } else {
-                    snake_head_position.0.x += 20.0
-                };
-            }
+            SnakeDirection::Up => snake_head_position.0.y += 1.0,
+            SnakeDirection::Down => snake_head_position.0.y -= 1.0,
+            SnakeDirection::Left => snake_head_position.0.x -= 1.0,
+            SnakeDirection::Right => snake_head_position.0.x += 1.0,
         }
+        /*
+               let mut temp_pos = snake_head_pos;
+               for mut snake_segment in &mut snake_segments.iter_mut() {
+                   let current_pos = snake_segment.0;
+                   snake_segment.0 = temp_pos;
+                   temp_pos = current_pos;
+               }
 
-           let mut temp_pos = snake_head_pos;
-            for mut snake_segment in &mut snake_segments.iter_mut() {
-                let current_pos = snake_segment.0;
-                snake_segment.0 = temp_pos;
-                temp_pos = current_pos;
-            }
+        */
     }
     Ok(())
 }
-fn project_positions(mut block_position: Query<(&mut Transform, &Position)>,) {
+fn project_positions(mut block_position: Query<(&mut Transform, &Position)>) {
     for (mut transform, position) in &mut block_position {
         transform.translation = position.0.extend(0.);
+    }
+}
+
+fn project_board(    board: Res<GameBoard>,mut block_position: Query<(&mut Transform, &GridLocation)>) {
+
+    let total_board_width = board.width as f32 * board.cell_size;
+    let total_board_height = board.height as f32 * board.cell_size;
+    let left_ofset = -total_board_width / 2.0;
+    let top_ofset = -total_board_height / 2.0;
+
+
+
+    for (mut transform, position) in &mut block_position {
+
+        transform.translation.x = left_ofset + (position.0.x * board.cell_size);
+        transform.translation.y = top_ofset + (position.0.y * board.cell_size);
+        transform.translation.z = 1.0;
+        println!("left offset = {:?}", left_ofset);
+        println!("top offset = {:?}", top_ofset);
+        println!("postion x = {:?}", left_ofset * position.0.x);
+        println!("postion y = {:?}", left_ofset * position.0.y);
+
     }
 }
 /*
@@ -157,3 +181,55 @@ fn change_speed(    keyboard_input: Res<ButtonInput<KeyCode>>,mut time: ResMut<T
     }
 }
 */
+
+fn draw_board(
+    board: Res<GameBoard>,
+    mut block_position: Query<(&Transform, &Position)>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    mut commands: Commands,
+) -> Result {
+    let total_board_width = board.width as f32 * board.cell_size;
+    let total_board_height = board.height as f32 * board.cell_size;
+    let window = window_query.single()?;
+    let left_ofset = -total_board_width / 2.0;
+    let top_ofset = -total_board_height / 2.0;
+
+    let shape = Rectangle::new(20.0, 20.0);
+    let mesh = meshes.add(shape);
+    //let colour = Color::Srgba(GREEN);
+    let green_material = materials.add(Color::Srgba(GREEN));
+    let light_green_material = materials.add(Color::Srgba(LIGHT_GREEN));
+/*
+    println!("window width = {:?}", window.width());
+    println!("total board width = {:?}", total_board_width);
+
+    println!("window height = {:?}", window.height());
+    println!("total board height = {:?}", total_board_height);
+
+    println!("left offset = {:?}", left_ofset);
+    println!("top offset = {:?}", top_ofset);
+*/
+    for x in 0..board.width {
+        for y in 0..board.height {
+            commands.spawn((
+                SnakeHead,
+                Mesh2d(mesh.clone()),
+                /*
+                if (x + y) % 2 == 0 {
+                    MeshMaterial2d(light_green_material.clone())
+                } else {
+                    MeshMaterial2d(green_material.clone())
+                },*/
+                MeshMaterial2d(light_green_material.clone()),
+                Position(Vec2::new(
+                    ((left_ofset / 2.0) + (x as f32 * 20.0)),
+                    (top_ofset + (y as f32 * 20.0)),
+                )),
+            ));
+        }
+    }
+
+    Ok(())
+}
